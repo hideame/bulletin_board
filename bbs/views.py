b066ccd83db3225e7f28500b795d355bb0cb0e93
hideame <hamezawa3@gmail.com>
2023-01-03 15:10:42 +0900

    create model

--- bbs/views.py
+++ bbs/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Article
 
 
 def index(request):
+    articles = Article.objects.all()
     context = {
         "message": "Welcome my BBS",
-        "players": ["バックエンドエンジニア", "フロントエンジニア", "SREエンジニア", "QAエンジニア"]
+        "articles": articles,
     }
     return render(request, "bbs/index.html", context)
+
+
+def detail(request, id):
+    article = get_object_or_404(Article, pk=id)
+    context = {
+        "message": "Show Article " + str(id),
+        "article": article,
+    }
+    return render(request, "bbs/detail.html", context)
